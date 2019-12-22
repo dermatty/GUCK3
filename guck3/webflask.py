c2d1e24be57ea5548863ce9eaf20b3506418cbec
dermatty <stephan@untergrabner.at>
2019-12-22 18:10:45 +0100

    proper logging in webflask

--- guck3/webflask.py
+++ guck3/webflask.py
@@ -2,31 +2,33 @@ from __future__ import unicode_literals
 import multiprocessing
 import gunicorn.app.base
 import os
-from flask import Flask, render_template, make_response, request, g, redirect, url_for, flash
+from flask import Flask, render_template, make_response, request, g, redirect, url_for, flash, session
+from flask.logging import default_handler
 from flask_sse import sse
 from flask_session import Session
 import flask_login
 from setproctitle import setproctitle
 from guck3.mplogging import whoami
-from guck3 import mplogging
 from guck3.g3db import G3DB
 import time
 from guck3 import models
+from threading import Thread
+import logging
 
 
 DB = None
-INQUEUE = None
-OUTQUEUE = None
-LOGGER = None
 USERS = None
 USERDATA = None
+DIRS = None
 
+
+# -------------- Helper functions --------------
 
 def number_of_workers():
     return (multiprocessing.cpu_count() * 2) + 1
 
 
-# init flask
+# -------------- Init Flask App --------------
 app = Flask(__name__)
 app.secret_key = "dfdsmdsv11nmDFSDfds"
 app.config['SESSION_TYPE'] = 'filesystem'
@@ -34,7 +36,23 @@ app.config["REDIS_URL"] = "redis://etec.iv.at"
 app.register_blueprint(sse, url_prefix='/stream')
 Session(app)
 
-# Login Manager
+
+# -------------- MainCommunicator --------------
+class MainCommunicator(Thread):
+
+    def __init__(self, inqueue, outqueue):
+        Thread.__init__(self)
+        self.daemon = True
+        self.inqueue = inqueue
+        self.outqueue = outqueue
+
+    def run(self):
+        while True:
+            time.sleep(1)
+
+
+# -------------- Login Manager --------------
+
 login_manager = flask_login.LoginManager()
 login_manager.login_view = 'userlogin'
 login_manager.init_app(app)
@@ -47,17 +65,16 @@ def beforerequest():
         g.user = user0
         if user0 is not None:
             USERDATA = DB.get_userdata()
-            if user0 not in USERDATA:
+            if not USERDATA or user0 not in USERDATA:
                 DB.insert_new_userdata(user0, time.time(), True, 0, [])
             else:
                 DB.update_userdata(user0, time.time(), True, USERDATA[user0]["no_newdetections"],
                                    USERDATA[user0]["photolist"])
     except Exception as e:
-        print(str(e))
+        app.logger.info(whoami() + str(e))
         pass
 
 
-# Login Manager
 class User(flask_login.UserMixin):
     pass
 
@@ -66,53 +83,70 @@ class User(flask_login.UserMixin):
 def user_loader(email):
     if email not in USERS:
         return
-    user = User()
-    user.id = email
+    try:
+        user = User()
+        user.id = email
+    except Exception:
+        pass
     return user
 
 
-@app.route("/", methods=['GET', 'POST'])
-@app.route("/home", methods=['GET', 'POST'])
-def index():
-    return render_template('index.html', userauth=flask_login.current_user.is_authenticated)
-
-
-@app.route("/detections", methods=['GET', 'POST'])
+@app.route("/userlogout", methods=['GET', 'POST'])
 @flask_login.login_required
-def detections():
-    return render_template('index.html', userauth=flask_login.current_user.is_authenticated)
+def userlogout():
+    userid = flask_login.current_user.get_id()
+    app.logger.info(whoami() + ": user logging out - " + userid)
+    flask_login.logout_user()
+    return redirect(url_for("index"))
 
 
 @app.route("/userlogin", methods=['GET', 'POST'])
 def userlogin():
     if request.method == "GET":
+        app.logger.info(whoami() + ": new user starting to log in ...")
         userloginform = models.UserLoginForm(request.form)
         return render_template("login.html", userloginform=userloginform, userauth=flask_login.current_user.is_authenticated)
     else:
         userloginform = models.UserLoginForm(request.form)
         email = userloginform.email.data
         pw = userloginform.password.data
+        app.logger.info(whoami() + ": user trying to log in - " + email)
         try:
             correct_pw = USERS[email]
         except Exception:
+            app.logger.warning(whoami() + ": user log in failed - " + email)
             return redirect(url_for("index"))
         if pw == correct_pw:
-            user = User()
-            user.id = email
-            flask_login.login_user(user)
+            app.logger.info(whoami() + ": user logged in - " + email)
+            try:
+                user = User()
+                user.id = email
+                flask_login.login_user(user)
+            except Exception:
+                pass
             return render_template("index.html")
+        app.logger.warning(whoami() + ": user log in failed- " + email)
         return redirect(url_for('index'))
-    return render_template('index.html', userauth=flask_login.current_user.is_authenticated)
 
 
-@app.route("/userlogout", methods=['GET', 'POST'])
+# -------------- Index.html / Home --------------
+
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/home", methods=['GET', 'POST'])
 @flask_login.login_required
-def userlogout():
-    global USER
-    flask_login.logout_user()
-    USER = None
-    return render_template("index.html", userauth=flask_login.current_user.is_authenticated)
+def index():
+    return render_template('index.html')
 
+
+# -------------- detections --------------
+
+@app.route("/detections", methods=['GET', 'POST'])
+@flask_login.login_required
+def detections():
+    return render_template('index.html')
+
+
+# -------------- StandaloneApplication/main --------------
 
 class StandaloneApplication(gunicorn.app.base.BaseApplication):
 
@@ -120,7 +154,6 @@ class StandaloneApplication(gunicorn.app.base.BaseApplication):
         self.options = options or {}
         self.application = app
         super().__init__()
-        # super(StandaloneApplication, self).__init__()
 
     def load_config(self):
         config = {key: value for key, value in self.options.items()
@@ -134,32 +167,34 @@ class StandaloneApplication(gunicorn.app.base.BaseApplication):
 
 def main(cfg, mplock, dirs, inqueue, outqueue, loggerqueue):
     global DB
-    global INQUEUE
-    global OUTQUEUE
-    global LOGGER
     global USERS
+    global USERDATA
 
     setproctitle("g3." + os.path.basename(__file__))
 
-    LOGGER = mplogging.setup_logger(loggerqueue, __file__)
-    LOGGER.info(whoami() + "starting ...")
+    log_handler = logging.FileHandler(dirs["logs"] + "webflask.log", mode="w")
+    log_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    app.logger.removeHandler(default_handler)
+    app.logger.setLevel(logging.DEBUG)
+    app.logger.addHandler(log_handler)
 
-    db = G3DB(mplock, cfg, dirs, LOGGER)
+    app.logger.info(whoami() + "starting ...")
+
+    db = G3DB(mplock, cfg, dirs, app.logger)
     DB = db
-    INQUEUE = inqueue
-    OUTQUEUE = outqueue
 
     # Password
     USERS = DB.get_users()
-    for key, item in USERS.items():
-        print(key, item)
     USERDATA = DB.get_userdata()
+
+    # start communicator thread
+    maincomm = MainCommunicator(inqueue, outqueue)
+    maincomm.start()
 
     options = {
         'bind': '%s:%s' % ('127.0.0.1', '8080'),
-        'error-logfile':  dirs["logs"] + "webflask_error.log",
-        'log-file': dirs["logs"] + "webflask.log",
         'capture-output': True,
+        'debug': True,
         'workers': number_of_workers(),
     }
     StandaloneApplication(app, options).run()
